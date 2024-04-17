d4815ccd468eb20a51f7d941411b92a27c5a6fdd
PanupongASsystem <PanupongI@assystem.co.th>
2024-04-17 14:56:58 +0700

    egp

--- application/views/frontend/home.php
+++ application/views/frontend/home.php
@@ -917,11 +917,11 @@
                                 <?= htmlspecialchars($data['project_name']) ?>
                             </a></span>
                     </div>
-                    <div class="d-flex justify-content-center" style="margin-top: -28px;">
+                    <!-- <div class="d-flex justify-content-center" style="margin-top: -28px;">
                         <a href="<?php echo site_url('Pages/e_gp'); ?>">
                             <img src="docs/k.btn-all.png">
                         </a>
-                    </div>
+                    </div> -->
                     <?php
                 endforeach;
             else: ?>
@@ -933,11 +933,11 @@
                 </div>
             <?php endif; ?>
 
-            <!-- <div class="d-flex justify-content-center" style="margin-top: -28px;">
+            <div class="d-flex justify-content-center" style="margin-top: -28px;">
                 <a href="<?php echo site_url('Pages/e_gp'); ?>">
                     <img src="docs/k.btn-all.png">
                 </a>
-            </div> -->
+            </div>
         </div>
         <div id="tab3" class="tab-content">
             <?php foreach ($qP_reb as $anou) { ?>
